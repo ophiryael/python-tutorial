1994bdffde3b97cf976b51f377e75f54ddea5acb
Ophir Yael <ofir.yael@gmail.com>
2020-08-02 18:00:30 +0300

    Add if and for examples

---
+++ for-stmt.py
@@ -0,0 +1,13 @@
+# Regular for statement
+words = ["cat", "window", "chair"]
+for w in words:
+    print(w, len(w))
+
+# range() function
+for i in range(0, 10, 3):
+    print(i)
+
+# Sequence indices
+a = ["Mary", "had", "a", "little", "lamb"]
+for i in range(len(a)):
+    print(i, a[i])
