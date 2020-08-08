d7b1703f9e3adf379a4174f5dae1ae660403f442
Ophir Yael <ofir.yael@gmail.com>
2020-08-08 16:52:51 +0300

    Add logging module example

---
+++ logs.py
@@ -0,0 +1,9 @@
+import logging
+
+logging.basicConfig(level=logging.DEBUG, filename="example.log")
+
+logging.debug("Debug log")
+logging.info("Info log")
+logging.warning("Warning log")
+logging.error("Error log")
+logging.critical("Critical log")
