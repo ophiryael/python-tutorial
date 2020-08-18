ebfbf3583a0524ba1168a0859b1d98092b1f7355
Ophir Yael <ofir.yael@gmail.com>
2020-08-18 19:56:43 +0300

    Refactor to run coroutines concurrently

--- asyncio/coroutines.py
+++ asyncio/coroutines.py
@@ -8,9 +8,11 @@ async def say_after(delay, what):
 
 
 async def main():
+    task1 = asyncio.create_task(say_after(1, "hello"))
+    task2 = asyncio.create_task(say_after(2, "world"))
     print(f"started at {time.strftime('%X')}")
-    await say_after(1, "hello")
-    await say_after(2, "world")
+    await task1
+    await task2
     print(f"finished at {time.strftime('%X')}")
 
 
