6bbfdaa6f179e1b7f992b639d4f7f974fe8b72b3
Ophir Yael <ofir.yael@gmail.com>
2020-08-23 19:36:51 +0300

    Add request with querystring example

--- requests_lib.py
+++ requests_lib.py
@@ -2,5 +2,8 @@ import requests
 
 # r = requests.get("https://api.github.com/events")
 
-r = requests.post("https://httpbin.org/post", data={"key": "value"})
+# r = requests.post("https://httpbin.org/post", data={"key": "value"})
 
+payload = {"key1": "value1", "key2": "value2", "key3": None}
+r = requests.get("https://httpbin.org/get", params=payload)
+print(r.url)
