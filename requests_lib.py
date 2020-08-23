6ab2fd33ddb59f6b3762b15a1cb5b8dc052825c2
Ophir Yael <ofir.yael@gmail.com>
2020-08-23 19:42:14 +0300

    Test multiple request props

--- requests_lib.py
+++ requests_lib.py
@@ -1,9 +1,14 @@
 import requests
 
 # r = requests.get("https://api.github.com/events")
+# print(r.encoding)
+# print(r.status_code)
+# print(r.text)
+# print(r.json())
 
 # r = requests.post("https://httpbin.org/post", data={"key": "value"})
 
-payload = {"key1": "value1", "key2": "value2", "key3": None}
-r = requests.get("https://httpbin.org/get", params=payload)
-print(r.url)
+# payload = {"key1": "value1", "key2": "value2", "key3": None}
+# r = requests.get("https://httpbin.org/get", params=payload)
+# print(r.url)
+
