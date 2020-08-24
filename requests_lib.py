497b5da567f8a7ae4dd1e6426231e3127e6779fd
Ophir Yael <ofir.yael@gmail.com>
2020-08-24 20:19:42 +0300

    Add json request body example

--- requests_lib.py
+++ requests_lib.py
@@ -6,13 +6,19 @@ import requests
 # print(r.text)
 # print(r.json())
 
+# as form
 # r = requests.post("https://httpbin.org/post", data={"key": "value"})
 
 # payload = {"key1": "value1", "key2": "value2", "key3": None}
 # r = requests.get("https://httpbin.org/get", params=payload)
 # print(r.url)
 
-url = "https://httpbin.org/get"
-headers = {"user-agent": "my-app/0.0.1"}
-r = requests.get(url, headers=headers)
-print(r.request.headers)
+# url = "https://httpbin.org/get"
+# headers = {"user-agent": "my-app/0.0.1"}
+# r = requests.get(url, headers=headers)
+# print(r.request.headers)
+
+url = "https://httpbin.org/post"
+payload = {"some_key": "some_data"}
+r = requests.post(url, json=payload)
+print(r.request.body)
